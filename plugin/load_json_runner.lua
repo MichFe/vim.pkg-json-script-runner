3db2c6956026910f07eab429c20a422838bdc758
Michelle Felix <mich.feloz@gmail.com>
2025-08-11 07:04:02 -0600

    chore: formatting

--- plugin/load_json_runner.lua
+++ plugin/load_json_runner.lua
@@ -1,3 +1,6 @@
-vim.api.nvim_create_user_command("NpmScripts", function()
-  require("script-runner").telescope_package_scripts()
-end, { desc = "Run npm scripts from package.json" })
+vim.api.nvim_create_user_command(
+  "NpmScripts",
+  function()
+    require("script-runner").telescope_package_scripts()
+  end, { desc = "Run npm scripts from package.json" }
+)
